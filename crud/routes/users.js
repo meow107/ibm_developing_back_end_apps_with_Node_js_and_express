7b07e79cdcc6c478d3321f2b7b67b315ca6f69dc
meow <46396000+meow107@users.noreply.github.com>
2025-10-16 16:44:04 +0700

    Add crud

---
+++ crud/routes/users.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+
+
+let users = [
+    {
+        firstName: "John",
+        lastName: "wick",
+        email:"johnwick@gamil.com",
+        DOB:"22-01-1990",
+    },
+    {
+        firstName: "John",
+        lastName: "smith",
+        email:"johnsmith@gamil.com",
+        DOB:"21-07-1983",
+    },
+    {
+        firstName: "Joyal",
+        lastName: "white",
+        email:"joyalwhite@gamil.com",
+        DOB:"21-03-1989",
+    },
+];
+
+// GET request: Retrieve all users
+router.get("/",(req,res)=>{
+  // Copy the code here
+  res.send(JSON.stringify({users},null,4))//This line is to be replaced with actual return value
+});
+
+// GET by specific ID request: Retrieve a single user with email ID
+router.get("/:email",(req,res)=>{
+  // Extract the email parameter from the request URL
+  const email = req.params.email;
+  // Filter the users array to find users whose email matches extracted email parameter
+  let fileter_users = users.filter((user) => user.email === email);
+  
+  if(fileter_users.length > 0) {
+    // Select the first matching user and update attributes if provided
+    let fileter_users = fileter_users[0];
+    
+    // Extract and update DOB if provided
+    let DOB = req.query.DOB;
+    if(DOB) {
+      fileter_users.DOB = DOB;
+    }
+
+    /*
+      Include similar code here for updating other attributes as needed
+     */
+
+    // Replace old user entry with updated user
+    users = users.filter((user) => user.email != email);
+    users.push(fileter_users);
+
+    // send sucess message indicating the use has been updated
+    res.send(`User with the emai ${email} updated.`);
+  }
+  else {
+    // send error message if no user found
+    res.send("Unable to find user!")
+  }
+});
+
+
+// POST request: Create a new user
+router.post("/",(req,res)=>{
+  // Push a new user object into the users array based on querry paramters from the request
+  users.push({
+    "firstName": req.query.firstName,
+    "lastName":req.query.lastName,
+    "email":req.query.email,
+    "DOB":req.query.DOB
+  });
+  // Send a success message as the reponse indicating the user has been added
+  res.send("The user " + req.query.firstName + " has been added!");
+});
+
+
+// PUT request: Update the details of a user by email ID
+router.put("/:email", (req, res) => {
+  // Copy the code here
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+});
+
+
+// DELETE request: Delete a user by email ID
+router.delete("/:email", (req, res) => {
+  // Extract the email paramater from the request URL
+  const email = req.params.email;
+
+  // Filter the users arrray to exclude the user with the specified email
+  users = users.filter((user) => user.email != email);
+
+  // Send a success message as the response, indicating the user has been deleted
+  res.send(`User with the email ${email} deleted.`);
+});
+
+module.exports=router;
